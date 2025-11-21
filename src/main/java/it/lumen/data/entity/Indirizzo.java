1ed34a45c881e1334300b2aa52b5c6c8755f3c40
gloriascarallo <g.scarallo1@studenti.unisa.it>
2025-11-21 18:59:06 +0100

    update Indirizzo.java

--- src/main/java/it/lumen/data/entity/Indirizzo.java
+++ src/main/java/it/lumen/data/entity/Indirizzo.java
@@ -2,6 +2,8 @@ package it.lumen.data.entity;
 
 import jakarta.persistence.*;
 
+import javax.validation.constraints.Pattern;
+
 @Entity
 @Table(name="Indirizzo")
 
@@ -18,11 +20,16 @@ public class Indirizzo {
     @Column(name="provincia", nullable=false, length=50)
     private String provincia;
 
+    @Pattern(regexp = "\\d{5}", message = "Il CAP deve avere 5 cifre")
+    @Column(name="cap", nullable=false, length=5)
+    private String cap;
+
     @Column(name="strada", nullable=false, length=255)
     private String strada;
 
     @Column(name="ncivico", nullable=true, length=10)
     private String NCivico;
+
 
     public Indirizzo(Integer IDIndirizzo, String citta, String provincia, String strada, String NCivico) {
         this.IDIndirizzo = IDIndirizzo;
