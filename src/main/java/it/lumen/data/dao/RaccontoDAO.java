b4da6f6f4b010796380f546913d00d05872ccc15
Michele98770 <michelechierchia2@gmail.com>
2025-11-22 10:58:43 +0100

    add AffiliazioneDAO, PartecipazioneDAO and RaccontoDAO

---
+++ src/main/java/it/lumen/data/dao/RaccontoDAO.java
@@ -0,0 +1,11 @@
+package it.lumen.data.dao;
+
+import it.lumen.data.entity.Racconto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface RaccontoDAO extends JpaRepository<Racconto, Integer> {
+
+    List<Racconto> findAllByUtente_Email(String email);
+}
