cd5764226d2a518709a3f2b31be3ebf660b59840
Michele98770 <michelechierchia2@gmail.com>
2025-11-22 18:32:03 +0100

    fix UtenteDAO.java function prototype

--- src/main/java/it/lumen/data/dao/UtenteDAO.java
+++ src/main/java/it/lumen/data/dao/UtenteDAO.java
@@ -5,5 +5,6 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 interface UtenteDAO extends JpaRepository<Utente, Integer>{
-    List <Utente> findallByUtente_ruolo(Utente.Ruolo ruolo);
+
+    List <Utente> findAllByRuolo(Utente.Ruolo ruolo);
 }
