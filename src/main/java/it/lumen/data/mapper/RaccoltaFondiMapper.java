a038ceb8453d5fad331aac153ded9fb67ac87dec
Santangelom3 <balotelligatto@gmail.com>
2025-11-22 17:35:29 +0100

    add RaccoltaFondiMapper.java

---
+++ src/main/java/it/lumen/data/mapper/RaccoltaFondiMapper.java
@@ -0,0 +1,25 @@
+package it.lumen.data.mapper;
+
+import it.lumen.data.dto.RaccoltaFondiDTO;
+import it.lumen.data.entity.RaccoltaFondi;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(
+        componentModel = "spring",
+        uses = {UtenteMapper.class},
+        unmappedTargetPolicy = ReportingPolicy.IGNORE
+)
+
+public interface RaccoltaFondiMapper {
+    @Mapping(source = "IDRaccolta", target = "idRaccoltaFondi")
+    @Mapping(source = "totaleraccolto", target = "totaleRaccolto")
+    @Mapping(source = "ente", target = "ente")
+    RaccoltaFondiDTO toDo(RaccoltaFondi entity);
+
+    @Mapping(source = "idRaccoltaFondi", target = "IDRaccolta")
+    @Mapping(source = "totaleRaccolto", target = "totaleraccolto")
+    RaccoltaFondi toEntity(RaccoltaFondiDTO dto);
+}
