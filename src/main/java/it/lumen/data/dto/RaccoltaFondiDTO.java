9e007d76de298a5146e32682d9edd23ab3bd74d8
Santangelom3 <balotelligatto@gmail.com>
2025-11-22 23:21:08 +0100

    refactor RaccoltaFondiDTO.java, RichiestaServizioDTO.java

--- src/main/java/it/lumen/data/dto/RaccoltaFondiDTO.java
+++ src/main/java/it/lumen/data/dto/RaccoltaFondiDTO.java
@@ -1,8 +1,8 @@
 package it.lumen.data.dto;
 
-import it.lumen.data.entity.RaccoltaFondi;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -11,28 +11,29 @@ import java.sql.Date;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class RaccoltaFondiDTO {
 
     @NotNull(message = "Un id è obbligatorio")
     private Integer idRaccoltaFondi;
 
-    @NotBlank(message = "È necessario un titolo per la raccolta fondi")
+    @NotBlank(message = "Un titolo è obbligatorio")
     @Size(max = 255)
     private String titolo;
 
     private String descrizione;
 
-    @NotBlank(message = "È necessario definire un obiettivo")
+    @NotNull(message = "Un obiettivo è obbligatorio")
     @Positive(message = "L'obiettivo deve essere maggiore di zero")
-    @Digits(integer = 8, fraction = 2, message = "Formato non valido")
+    @Digits(integer = 8, fraction = 2, message = "Formato dell'obiettivo non valido")
     private BigDecimal obiettivo;
 
-    @Digits(integer = 8, fraction = 2, message = "Formato non valido")
+    @Digits(integer = 8, fraction = 2, message = "Formato del totale raccolto non valido")
     private BigDecimal totaleRaccolto;
 
-    @NotBlank(message = "La data di apertura è obbligatoria")
+    @NotNull(message = "La data di apertura è obbligatoria")
     @FutureOrPresent(message = "La data di apertura non può essere nel passato")
-    @JsonFormat(pattern = "dd-MM-yyy")
+    @JsonFormat(pattern = "dd-MM-yyyy")
     private Date dataApertura;
 
     @NotNull(message = "La data di chiusura è obbligatoria")
@@ -40,7 +41,7 @@ public class RaccoltaFondiDTO {
     @JsonFormat(pattern = "dd-MM-yyyy")
     private Date dataChiusura;
 
+    @NotBlank(message = "L'email di un ente è obbligatoria")
     @Email(message = "Email dell'ente non valida")
-    @NotBlank(message = "Specificare l'Ente")
     private String ente;
 }
--- src/main/java/it/lumen/data/dto/RichiestaServizioDTO.java
+++ src/main/java/it/lumen/data/dto/RichiestaServizioDTO.java
@@ -3,34 +3,37 @@ package it.lumen.data.dto;
 import it.lumen.data.entity.RichiestaServizio;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.sql.Date;
 
-
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class RichiestaServizioDTO {
 
-    @NotNull(message = "Va specificato un ID")
+    @NotNull(message = "Un id è obbligatorio")
     private Integer idRichiestaServizio;
 
     @NotBlank(message = "Il messaggio della richiesta è obbligatorio")
     private String testo;
 
-    @NotBlank(message = "Deve essere specificata la data di creazione della richiesta")
-    //@FutureOrPresent(message = "La data di creazione della richiesta non può essere nel passato")
+    @NotNull(message = "La data della creazione della richiesta è obbligatoria")
     @JsonFormat(pattern = "dd-MM-yyyy")
     private Date dataRichiesta;
 
     @NotBlank(message = "Lo stato della richiesta è obbligatorio")
     private RichiestaServizio.StatoRichiestaServizio stato;
 
+    @NotBlank(message = "L'email del beneficiario è obbligatoria")
     @Email(message = "Email del beneficiario non valida")
     private String beneficiario;
 
+    @NotBlank(message = "L'email del destinatario è obbligatoria")
     @Email(message = "Email del destinatario non valida")
     private String enteVolontario;
 }
