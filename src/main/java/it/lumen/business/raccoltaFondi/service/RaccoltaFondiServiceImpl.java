353472f44a1c17a308753c7cf243d279210ad4d5
Santangelom3 <balotelligatto@gmail.com>
2025-11-23 23:56:49 +0100

    add RaccoltaFondiService.java, RaccoltaFondiServiceImpl.java

---
+++ src/main/java/it/lumen/business/raccoltaFondi/service/RaccoltaFondiServiceImpl.java
@@ -0,0 +1,37 @@
+package it.lumen.business.raccoltaFondi.service;
+
+import it.lumen.data.dao.RaccoltaFondiDAO;
+import it.lumen.data.entity.RaccoltaFondi;
+
+import it.lumen.data.entity.Utente;
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import jakarta.transaction.Transactional;
+
+import java.util.List;
+
+@Service
+public class RaccoltaFondiServiceImpl implements RaccoltaFondiService {
+
+    private final RaccoltaFondiDAO  raccoltaFondiDAO;
+
+    @Autowired
+    public RaccoltaFondiServiceImpl(RaccoltaFondiDAO raccoltaFondiDAO) {this.raccoltaFondiDAO = raccoltaFondiDAO;}
+
+    @Override
+    @Transactional
+    public void avviaRaccoltaFondi(RaccoltaFondi raccoltaFondi){raccoltaFondiDAO.save(raccoltaFondi);}
+
+    @Override
+    @Transactional
+    public void terminaRaccoltaFondi(RaccoltaFondi raccoltaFondi){raccoltaFondiDAO.save(raccoltaFondi);}
+
+    @Override
+    public List <RaccoltaFondi> ottieniRaccolteDiEnte(Utente utente){
+        return raccoltaFondiDAO.findAllByEnte_Email(utente.getEmail());
+    }
+
+
+
+}
+
--- src/main/java/it/lumen/data/entity/RaccoltaFondi.java
+++ src/main/java/it/lumen/data/entity/RaccoltaFondi.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
