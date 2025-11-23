32ae5ca91d47a118d2633718e3c1bf7b977207d0
Michele98770 <michelechierchia2@gmail.com>
2025-11-23 21:18:58 +0100

    add GestioneAccountService.java, GestioneAccountServiceImpl.java

---
+++ src/main/java/it/lumen/business/gestioneAccount/service/GestioneAccountService.java
@@ -0,0 +1,7 @@
+package it.lumen.business.gestioneAccount.service;
+
+import it.lumen.data.entity.Utente;
+
+public interface GestioneAccountService {
+    public void modificaUtente(Utente utente);
+}
