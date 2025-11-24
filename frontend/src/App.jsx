f2b6abd208a70d2030558ecc375d878c9889c493
giovanni-s1919 <gianniscardone96@gmail.com>
2025-11-24 13:01:06 +0100

    delated profilo ente and related pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Navbar from './components/Navbar';
-import EventCard from './components/EventCard';
 import BeneficiarioProfile from './pages/BeneficiarioProfile';
 import EnteProfile from './pages/EnteProfile';
 import VolontarioProfile from './pages/VolontarioProfile'
--- frontend/src/components/TastoProva.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-export default function TastoProfilo() {
-  return (
-    <a href="./pages/ProfiloEnte.jsx">
-      <button>Vai al Profilo</button>
-    </a>
-  );
-}
