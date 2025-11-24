05ba3423631b40481e39aa7f6bdb1b96ed1c234c
denysvit <denysvit@pm.me>
2025-11-24 16:47:55 +0100

    add gestionePartecipazioneEvento control

---
+++ src/main/java/it/lumen/business/gestionePartecipazioneEvento/control/gestionePartecipazioneEventoControl.java
@@ -0,0 +1,67 @@
+package it.lumen.business.gestionePartecipazioneEvento.control;
+
+import it.lumen.business.gestioneAccount.service.GestioneAccountService;
+import it.lumen.business.gestionePartecipazioneEvento.service.PartecipazioneEventoService;
+import it.lumen.data.entity.Evento;
+import it.lumen.data.entity.Partecipazione;
+import it.lumen.data.entity.Utente;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.validation.BindingResult;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/partecipazione")
+public class gestionePartecipazioneEventoControl {
+
+	@Autowired
+    private PartecipazioneEventoService partecipazioneEventoService;
+
+
+	@PostMapping("/aggiungi")
+	public ResponseEntity<String> aggiungiPartecipazione(@RequestBody Partecipazione partecipazione){
+
+		Evento evento = partecipazione.getEvento();
+		Utente volontario = partecipazione.getVolontario();
+
+		try{
+
+			if(evento == null){
+
+                return new ResponseEntity<>("Evento non può essere vuoto", HttpStatus.BAD_REQUEST);
+			}
+			if(volontario == null){
+
+                return new ResponseEntity<>("Volontario non può essere vuoto", HttpStatus.BAD_REQUEST);
+			}
+
+			ArrayList<Partecipazione> listaPartecipazioniEvento = partecipazioneEventoService.listaPartecipazioni(evento.getIdEvento());
+
+			if(listaPartecipazioniEvento.contains(volontario)){
+
+				return new ResponseEntity<>("Volontario gia' partecipa all'evento", HttpStatus.BAD_REQUEST);
+			}
+
+			if(listaPartecipazioniEvento.size() >= evento.getMaxPartecipanti()){
+
+				return new ResponseEntity<>("Numero di partecipanti al completo", HttpStatus.BAD_REQUEST);
+			}
+
+			partecipazione.setData(new Date(System.currentTimeMillis()));
+			partecipazioneEventoService.aggiungiPartecipazione(partecipazione);
+
+		}catch(Exception e){
+
+            return new ResponseEntity<>("Errore interno del server " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+	// TODO: modifica and rimuovi
+}
