fb0b81e961e3793a54994b05bcde493acb891b20
giovanni-s1919 <gianniscardone96@gmail.com>
2025-11-25 19:38:58 +0100

    base for user pages

---
+++ frontend/src/pages/ProfiloVolontario.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+import ProfileInfo from '../components/ProfileInfo';
+import Footer from '../components/Footer';
+import '../stylesheets/ProfiloVolontario.css';
+
+const ProfiloVolontario = () => {
+  return (
+    <div className="voluntary-page-wrapper">
+      <Navbar />
+      <div className="voluntary-container">
+        <section className="profile-section-full">
+          <ProfileInfo />
+        </section>
+        <section className="future-content-section">
+        </section>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default ProfiloVolontario;
