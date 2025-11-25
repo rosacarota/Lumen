8d1fbe030c7975ec83cbb384ac4206db78685a0b
giovanni-s1919 <gianniscardone96@gmail.com>
2025-11-25 20:02:15 +0100

    .

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import AccessoProfiloVolontario from './pages/AccessoProfiloVolontario';
 
 const App = () => {
   return (
     <div>
-      <AccessoProfiloVolontario />
+      
     </div>
   )
 }
