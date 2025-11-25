8a7acd9b81a18a4c7568adcc149a2a99b3b435b6
gloriascarallo <g.scarallo1@studenti.unisa.it>
2025-11-25 12:41:25 +0100

    update GestioneRaccontoServiceImpl AND GestioneRaccontoService.java GestioneRaccontoControl.java AND Racconto.java AND RaccontoDAO.java

--- src/main/java/it/lumen/business/gestioneRacconto/control/GestioneRaccontoControl.java
+++ src/main/java/it/lumen/business/gestioneRacconto/control/GestioneRaccontoControl.java
@@ -9,85 +9,104 @@ import it.lumen.data.entity.Utente;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.sql.Date;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/racconto")
 public class GestioneRaccontoControl {
 
-    @Autowired
-    private RaccontoDAO raccontoDAO;
 
     @Autowired
     private GestioneRaccontoService gestioneRaccontoService;
-
 
     @PostMapping("/aggiungi")
     public ResponseEntity<String> aggiuntaRacconto(@RequestBody Racconto racconto) {
 
         try {
 
-            if(racconto.getTitolo()==null) {
+            /*
+            if (result.hasErrors()) {
+                StringBuilder errorMsg = new StringBuilder("Errori di validazione:");
+                result.getAllErrors().forEach(e -> errorMsg.append(" ").append(e.getDefaultMessage()));
+                return ResponseEntity.badRequest().body(errorMsg.toString());
+            }
+            */
+
+            if (racconto.getTitolo() == null) {
 
                 return new ResponseEntity<>("Titolo non può essere vuoto", HttpStatus.BAD_REQUEST);
             }
 
-            if(racconto.getDescrizione()==null) {
+            if (racconto.getDescrizione() == null) {
 
                 return new ResponseEntity<>("Descrizione non può essere vuota", HttpStatus.BAD_REQUEST);
             }
 
-            if(racconto.getUtente()==null) {
+
+            if (racconto.getUtente() == null) {
 
                 return new ResponseEntity<>("Email dell'utente non può essere vuota", HttpStatus.BAD_REQUEST);
             }
 
-            racconto.setDataPubblicazione(new Date(System.currentTimeMillis()));
-            Racconto raccontoSalvato = gestioneRaccontoService.aggiungiRacconto(racconto);
-            if (raccontoSalvato != null) {
+            // dovrei controllare anche che l'utente esista o lo prendo dalla sessione?
 
+            racconto.setDataPubblicazione(new Date(System.currentTimeMillis()));
+            Racconto raccontoAggiunto = gestioneRaccontoService.aggiungiRacconto(racconto);
+            if (raccontoAggiunto != null) {
                 return new ResponseEntity<>("Aggiunta del racconto avvenuta con successo.", HttpStatus.CREATED);
 
             } else {
 
                 return new ResponseEntity<>("Si è verificato un errore nell'aggiunta del racconto.", HttpStatus.BAD_REQUEST);
             }
-        } catch (Exception e) {
 
+        } catch (Exception e) {
             return new ResponseEntity<>("Errore interno del server " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
+
     @PostMapping("/modifica")
-    public ResponseEntity<String> modificaRacconto(@RequestParam Integer idRacconto, @RequestBody Racconto nuovoRacconto) {
+    public ResponseEntity<String> modificaRacconto(@RequestBody Racconto nuovoRacconto) {
 
         try {
+            Integer idRacconto=nuovoRacconto.getIdRacconto();
+            if (idRacconto == null) {
+                return new ResponseEntity<>("IdRacconto non può essere vuoto", HttpStatus.BAD_REQUEST);
 
-            if(nuovoRacconto.getTitolo()==null) {
+            }
+
+            if (nuovoRacconto.getTitolo() == null) {
 
                 return new ResponseEntity<>("Titolo non può essere vuoto", HttpStatus.BAD_REQUEST);
             }
 
-            if(nuovoRacconto.getDescrizione()==null) {
+            if (nuovoRacconto.getDescrizione() == null) {
 
                 return new ResponseEntity<>("Descrizione non può essere vuota", HttpStatus.BAD_REQUEST);
             }
 
-            if(nuovoRacconto.getUtente()==null) {
+
+            if (nuovoRacconto.getUtente() == null) {
 
                 return new ResponseEntity<>("Email dell'utente non può essere vuota", HttpStatus.BAD_REQUEST);
             }
 
-        Racconto raccontoDaModificare= raccontoDAO.getRaccontoByIdRacconto(idRacconto);
+            if(nuovoRacconto.getDataPubblicazione()==null) {
 
-        if(raccontoDaModificare==null) {
+                return new ResponseEntity<>("Data di pubblicazione del racconto non può essere vuota", HttpStatus.BAD_REQUEST);
+            }
 
-            return new ResponseEntity<>("Racconto da modificare non trovato.", HttpStatus.NOT_FOUND);
-        }
-
+            if (!gestioneRaccontoService.checkId(idRacconto)) {
+                return new ResponseEntity<>("Racconto da modificare non trovato", HttpStatus.NOT_FOUND);
+            }
+/*
         if (nuovoRacconto.getUtente() != null &&
                 !nuovoRacconto.getUtente().getEmail().equals(raccontoDaModificare.getUtente().getEmail())) {
             return new ResponseEntity<>("Non è possibile modificare l'utente del racconto.", HttpStatus.FORBIDDEN);
@@ -98,35 +117,59 @@ public class GestioneRaccontoControl {
         raccontoDaModificare.setTitolo(nuovoRacconto.getTitolo());
         raccontoDaModificare.setDescrizione(nuovoRacconto.getDescrizione());
         raccontoDaModificare.setImmagine(nuovoRacconto.getImmagine());
+*/
 
-        Racconto raccontoModificato=gestioneRaccontoService.modificaRacconto(raccontoDaModificare);
-        if(raccontoModificato!=null) {
-            return new ResponseEntity<>("Modifica del racconto avvenuta con successo.", HttpStatus.CREATED);
+// check per vedere se email del racconto corrisponde ad email in sessione
+            Racconto raccontoModificato = gestioneRaccontoService.modificaRacconto(nuovoRacconto);
+            if (raccontoModificato != null) {
+                return new ResponseEntity<>("Modifica del racconto avvenuta con successo.", HttpStatus.CREATED);
 
-        } else {
+            } else {
 
-            return new ResponseEntity<>("Si è verificato un errore nella modifica del racconto.", HttpStatus.BAD_REQUEST);
+                return new ResponseEntity<>("Si è verificato un errore nella modifica del racconto.", HttpStatus.BAD_REQUEST);
+            }
+        } catch (Exception e) {
+
+            return new ResponseEntity<>("Errore interno del server " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
-    } catch (Exception e) {
 
-        return new ResponseEntity<>("Errore interno del server " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
-
-        }
 
 
     @PostMapping("/rimuovi")
-    public ResponseEntity<String> rimuoviRacconto(@RequestParam @Valid Integer idRacconto) {
+    public ResponseEntity<String> rimuoviRacconto(@RequestBody Map<String, Integer> body) {
 
-        Racconto raccontoDaEliminare=raccontoDAO.getRaccontoByIdRacconto(idRacconto);
-        if(raccontoDaEliminare==null) {
 
-            return new ResponseEntity<>("Racconto da eliminare non trovato.", HttpStatus.NOT_FOUND);
-        }
+try {
+    Integer idRacconto=body.get("idRacconto");
+    if (idRacconto == null) {
+        return new ResponseEntity<>("IdRacconto non può essere vuoto", HttpStatus.BAD_REQUEST);
+
+    }
+
+    if (!gestioneRaccontoService.checkId(idRacconto)) {
+        return new ResponseEntity<>("Racconto da eliminare non trovato", HttpStatus.NOT_FOUND);
+    }
+
+    // check per vedere se email del racconto corrisponde ad email in sessione
 
         gestioneRaccontoService.eliminaRacconto(idRacconto);
         return new ResponseEntity<>("Racconto eliminato con successo.", HttpStatus.OK);
+    } catch (Exception e) {
+        return new ResponseEntity<>("Errore interno del server " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+    }
+
+
+    @PostMapping("/visualizza")
+    public ResponseEntity<List<Racconto>> visualizzaRaccontiUtente(@RequestBody Map<String, String> body) {
+
+        String email=body.get("email");
+        List<Racconto> lista = gestioneRaccontoService.listaRaccontiUtente(email);
+
+        return ResponseEntity.ok(lista);
 
     }
 
 }
+
--- src/main/java/it/lumen/business/gestioneRacconto/service/GestioneRaccontoServiceImpl.java
+++ src/main/java/it/lumen/business/gestioneRacconto/service/GestioneRaccontoServiceImpl.java
@@ -2,9 +2,14 @@ package it.lumen.business.gestioneRacconto.service;
 
 import it.lumen.data.dao.RaccontoDAO;
 import it.lumen.data.entity.Racconto;
+import it.lumen.data.entity.Utente;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class GestioneRaccontoServiceImpl implements GestioneRaccontoService {
@@ -35,5 +40,24 @@ public class GestioneRaccontoServiceImpl implements GestioneRaccontoService {
     public void eliminaRacconto(Integer idRacconto) {
 
         raccontoDAO.removeByIdRacconto(idRacconto);
+    }
+
+    public Racconto getByIdRacconto(int idRacconto) {
+
+        return raccontoDAO.getRaccontoByIdRacconto(idRacconto);
+
+    }
+
+    public boolean checkId(int idRacconto) {
+
+        return raccontoDAO.existsById(idRacconto);
+
+    }
+
+    public List<Racconto> listaRaccontiUtente(String email) {
+
+        return raccontoDAO.findAllByUtente_Email(email);
+
+
     }
 }
