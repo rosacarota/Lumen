aa11c5187165d0d758de1cdb1b20a888ac79e82b
Luca Afeltra <problast666@gmail.com>
2025-11-27 12:09:10 +0100

    add routing to pages in App.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,14 +3,11 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
-import LoginPage from './pages/LoginPage.jsx'
-import EnteProfile from './pages/EnteProfile.jsx'
-import Raccoltafondi from './pages/Raccoltafondi.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode> 
     <BrowserRouter>
-      <App/>
+      <App />
     </BrowserRouter>
-  </StrictMode>,
+  </StrictMode>
 )
