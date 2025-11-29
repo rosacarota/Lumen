a759b98890259aff7fb6a99240cbfb31b9ffd92a
Luca Afeltra <problast666@gmail.com>
2025-11-29 22:28:40 +0100

    update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -157,7 +157,7 @@ const DropdownMenu = ({ role, onLogout }) => {
           { label: 'Area Personale', icon: <Settings size={16} />, href: '/profiloente' },
           { label: 'Gestione eventi', icon: <Calendar size={16} />, href: '/eventi-gestione' },
           { label: 'Gestione raccolte fondi', icon: <Heart size={16} />, href: '/raccolte-fondi' },
-          { label: 'Gestione affiliati', icon: <Users size={16} />, href: '/affiliati' },
+          { label: 'Gestione affiliati', icon: <Users size={16} />, href: '/DashboardAffiliazione' },
           { label: 'Logout', icon: <LogOut size={16} />, action: onLogout, type: 'danger' }
         ];
       case 'guest':
