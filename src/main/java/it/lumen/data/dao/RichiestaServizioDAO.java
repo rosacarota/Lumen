7d3a272cbcdce98bb8f9f2f3cab49781cc0aee2f
Santangelom3 <balotelligatto@gmail.com>
2025-12-01 17:37:42 +0100

    fix RichiestaServizioDAO.java

--- src/main/java/it/lumen/data/dao/RichiestaServizioDAO.java
+++ src/main/java/it/lumen/data/dao/RichiestaServizioDAO.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface RichiestaServizioDAO extends JpaRepository<RichiestaServizio, Integer>{
-    List<RichiestaServizio>findAllByEnteVolontraio_Email(String email);           //----TO CHECK
+    List<RichiestaServizio>findAllByEnteVolontario_Email(String email);           //----TO CHECK
     List<RichiestaServizio>findAllByBeneficiario_Email(String email);
 }
