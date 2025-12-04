6e4c4b4be56922c6c82611e2643f1908c9f7bb56
giovanni-s1919 <gianniscardone96@gmail.com>
2025-12-04 14:54:27 +0100

    edit ProfiloVolontario.jsx + ProfiloVolontario.css: fixed Bacheca Racconti and added Cronologia Eventi

--- frontend/src/pages/ProfiloVolontario.jsx
+++ frontend/src/pages/ProfiloVolontario.jsx
@@ -1,25 +1,31 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 import AccessoInfoProfilo from '../components/AccessoInfoProfilo';
-import BachecaUltimeStorie from '../components/BachecaUltimeStorie';
+import BachecaRacconti from '../components/BachecaRacconti';
+import CronologiaEventi from '../components/CronologiaEventi';
 import Footer from '../components/Footer';
 import '../stylesheets/ProfiloVolontario.css';
 
 const ProfiloVolontario = () => {
-  return (
-    <div className="voluntary-page-wrapper">
-      <Navbar />
-      <div className="voluntary-container">
-        <section className="profile-section-full">
-          <AccessoInfoProfilo />
-        </section>
-        <section className="voluntary-stories-section">
-          <BachecaUltimeStorie />
-        </section>
-      </div>
-      <Footer />
-    </div>
-  );
+    return (
+        <div className="voluntary-page-wrapper">
+            <Navbar />
+            <div className="voluntary-container">
+                <section className="profile-section-full">
+                    <AccessoInfoProfilo />
+                </section>
+                <div className="voluntary-split-layout">
+                    <section className="voluntary-stories-section">
+                        <BachecaRacconti />
+                    </section>
+                    <section className="voluntary-events-section">
+                        <CronologiaEventi />
+                    </section>
+                </div>
+            </div>
+            <Footer />
+        </div>
+    );
 };
 
 export default ProfiloVolontario;
