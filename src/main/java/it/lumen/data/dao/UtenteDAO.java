1e951f602552ddb1fa967a51511da638bea517bb
Santangelom3 <balotelligatto@gmail.com>
2025-12-05 20:24:00 +0100

    update UtenteDAO.java

--- src/main/java/it/lumen/data/dao/UtenteDAO.java
+++ src/main/java/it/lumen/data/dao/UtenteDAO.java
@@ -3,11 +3,13 @@ package it.lumen.data.dao;
 import it.lumen.data.entity.Utente;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UtenteDAO extends JpaRepository<Utente,String>{
 
     Utente findByEmail(String email);
 
+    @Query("SELECT u FROM Utente u WHERE u.nome ILIKE %?1%")
     List<Utente> findUtentiByNome(String nome);
 
     boolean existsByEmail(String email);
