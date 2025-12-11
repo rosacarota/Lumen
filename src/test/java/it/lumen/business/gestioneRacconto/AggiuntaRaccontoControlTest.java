a9073cf29deb70aeb5489ce2d0c3c8a07d5f6387
gloriascarallo <g.scarallo1@studenti.unisa.it>
2025-12-11 11:02:12 +0100

    update AggiuntaRaccontoControlTest.java

--- src/test/java/it/lumen/business/gestioneRacconto/AggiuntaRaccontoControlTest.java
+++ src/test/java/it/lumen/business/gestioneRacconto/AggiuntaRaccontoControlTest.java
@@ -1,6 +1,5 @@
 package it.lumen.business.gestioneRacconto;
 
-import org.junit.jupiter.api.*;
 import it.lumen.business.gestioneAutenticazione.service.AutenticazioneService;
 import it.lumen.business.gestioneRacconto.control.GestioneRaccontoControl;
 import it.lumen.business.gestioneRacconto.service.GestioneRaccontoService;
@@ -15,7 +14,6 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.lenient;
@@ -73,7 +71,7 @@ import static org.mockito.Mockito.when;
             String titoloLungo= "Vi racconto la storia della mia vita, da quando da giovane lasciai il mio piccolo paese tra le montagne del sud sperando in un futuro migliore, ma non riuscii mai a dimenticare la povertà che divorava la mia terra d’origine, le famiglie senza lavoro, i bambini senza scuole e le strade che cadevano a pezzi mentre lottavo per cambiare il destino della mia gente.";
             racconto.setTitolo(titoloLungo);
 
-            ResponseEntity<?> response = gestioneRaccontoControl.aggiuntaRacconto(racconto, token);
+            ResponseEntity<Racconto> response = gestioneRaccontoControl.aggiuntaRacconto(racconto, token);
 
             assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
 
@@ -104,20 +102,19 @@ import static org.mockito.Mockito.when;
 
         // TC_1.2.1_4: Immagine formato non valido
         @Test
-        void testModificaEvento_ImmagineFormatoErrato() {
+        void testAggiuntaRacconto_ImmagineFormatoErrato() {
             String mp4Base64 = "data:video/mp4;base64,AAAAHGZ0eXBNU";
             racconto.setImmagine(mp4Base64);
 
             ResponseEntity<Racconto> response = gestioneRaccontoControl.aggiuntaRacconto(racconto, token);
 
-            // Oracle: La modifica non va a buon fine
             assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
         }
 
-        // TC_1.3.3_5: Racconto valido
+        // TC_1.2.1_5: Racconto valido
         @Test
-        void testModificaEvento_RaccontoValido() {
-            // immagine valida jpg set in setUp
+        void testAggiuntaRacconto_RaccontoValido() {
+
             when(gestioneRaccontoService.aggiungiRacconto(any(Racconto.class))).thenReturn(racconto);
 
             ResponseEntity<Racconto> response = gestioneRaccontoControl.aggiuntaRacconto(racconto, token);
